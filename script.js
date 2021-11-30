98cf2c4d1a4b767964317d7ad8bfc1352ebea68d
stantoha <78742661+stantoha@users.noreply.github.com>
2021-11-30 17:36:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let button=document.querySelector('button');
+let nav=document.querySelector('nav')
+
+
+    button.addEventListener(onclick,function() {
+            nav.classList.add('background');
+        });
+
